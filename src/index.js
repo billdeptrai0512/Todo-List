4aba2b96e44616f0fa433e39d0953ec39e3e25dc
billdeptrai0512 <billnguyen051298@gmail.com>
2023-12-18 17:16:35 +0700

    refactor the file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import { showAllTask } from './alltask';
 import { showAllIdea } from './allIdea';
 import { showImportant } from './important';
+import { addToday } from './today';
 
 // make the left panel disappear and appear
 const leftPanel = document.querySelector('.left');
@@ -70,264 +71,7 @@ prevNextIcon.forEach(icon => {
     })
 })
 
-//make default right panel is today
-const title = document.getElementById('title');
-title.innerHTML = new Date().toLocaleDateString('en-GB')
-
-const today = document.getElementById('Today');
-today.classList.add("active"),
-today.addEventListener('click', () => {
-    title.innerHTML = ''
-    title.innerHTML = new Date().toLocaleDateString('en-GB')
-    today.classList.add("active")
-    allTask.classList.remove("active")
-    allIdea.classList.remove("active")
-    important.classList.remove("active")
-})
-
-// add eventlistener to create new right panel
-const allTask = document.getElementById('Task');
-allTask.addEventListener('click',  () => {
-    showAllTask()
-    today.classList.remove("active")
-    allTask.classList.add("active")
-    allIdea.classList.remove("active")
-    important.classList.remove("active")
-});
-
-const allIdea = document.getElementById('Idea');
-allIdea.addEventListener('click', () => {
-    showAllIdea();
-    today.classList.remove("active")
-    allTask.classList.remove("active")
-    allIdea.classList.add("active")
-    important.classList.remove("active")
-});
-
-const important = document.getElementById('Important');
-important.addEventListener('click', () => {
-    showImportant();
-    today.classList.remove("active")
-    allTask.classList.remove("active")
-    allIdea.classList.remove("active")
-    important.classList.add("active")
-});
-
-// click add task / add idea will remove the hidden class
-const listItem = document.getElementById('listTask')
-const formTask = document.getElementById("task")
-
-const buttonAddTask = document.getElementById("addTask")
-buttonAddTask.addEventListener('click', () => {
-    formTask.classList.remove('hidden')
-})
-
-const cancelTaskButton = document.querySelector('input[id="cancel"]')
-cancelTaskButton.addEventListener('click', () => {
-    formTask.classList.add('hidden')
-})
-
-function createEditForm() {
-    const place = document.querySelector('.form')
-    
-    const form = document.createElement('form')
-    form.id = "editTask"
-
-    const title = document.createElement('label')
-    title.setAttribute('for', 'title')
-    title.textContent = "Title:"
-    form.appendChild(title)
-
-    const input = document.createElement('input')
-    input.setAttribute('type', 'text')
-    input.setAttribute('name', 'title')
-    input.setAttribute('placeholder', 'What you gonna do ?')
-    input.id = "inputEdit"
-    form.appendChild(input)
-
-    const details = document.createElement('label')
-    details.setAttribute('for', 'details')
-    details.textContent = "Details:"
-    form.appendChild(details)
-
-    const textArea = document.createElement('textarea')
-    textArea.setAttribute('rows', '2')
-    textArea.setAttribute('cols', '50')
-    textArea.setAttribute('name', 'details')
-    textArea.setAttribute('placeholder', 'Anything that make you understand why you have to do it')
-    textArea.id = "detailsEdit"
-    form.appendChild(textArea)
-
-    const twoButton = document.createElement('div')
-    twoButton.id = "button"
-
-    const submit = document.createElement('input')
-    submit.setAttribute('type', 'submit')
-    submit.setAttribute('name', 'submit')
-    submit.setAttribute('value', 'Edit')
-    submit.id = "edit"
-    twoButton.appendChild(submit)
-
-    const cancel = document.createElement('input')
-    cancel.setAttribute('type', 'button')
-    cancel.setAttribute('name', 'cancel')
-    cancel.setAttribute('value', 'Cancel')
-    cancel.id = "editCancel"
-    twoButton.appendChild(cancel)
-
-    form.appendChild(twoButton)
-    place.appendChild(form)
-}
-
-let newItem = (function newItem () {
-    const baseID = 'item'
-    let count = 0
-    return function (type = 'div') {
-        let item = document.createElement(type)
-        item.classList = baseID
-        item.id = count
-        count += 1
-        return item
-    }
-}())
-
-// Design a div to represent the task/idea div
-function createItem(listItem, task) {
-
-    const item = newItem('div')
-    const itemID = Number(item.id)
-
-    const done = document.createElement('input')
-    done.type = "checkbox";
-    done.name = "done"
-    done.setAttribute('id', 'done')
-    item.appendChild(done)
-
-    done.defaultChecked = task.done == true ? true : false
-
-    //make button done change status
-    done.addEventListener('click', () => {
-        task.beDone()
-    })
-
-    let text = document.createElement('div')
-    text.setAttribute('id', 'text')
-    let h3 = document.createElement('h3')
-    h3.innerHTML = task.title
-    text.appendChild(h3)
-    let p = document.createElement('p')
-    p.innerHTML = task.describtion
-    text.appendChild(p)
-    item.appendChild(text)
-
-    let combo = document.createElement('div')
-    combo.id = "combo"
-
-    const star = document.createElement('div')
-    star.classList = 'star' 
-    const starBox = document.createElement('input')
-    starBox.type = "checkbox"
-    starBox.setAttribute('id', `important${itemID}`)
-    const starLabel = document.createElement('label')
-    starLabel.setAttribute('for', `important${itemID}`)
-    star.appendChild(starBox)
-    star.appendChild(starLabel)
-    combo.appendChild(star)
-
-    starBox.defaultChecked = task.important == true ? true : false
-
-    starBox.addEventListener('click', () => {
-        task.beImportant()
-    })
-
-    const edit = document.createElement('div')
-    edit.setAttribute('id', 'editContainer')
-    const editSpan = document.createElement('span')
-    editSpan.setAttribute('id', 'vert')
-    editSpan.classList = "material-symbols-outlined"
-    editSpan.textContent = "more_vert"
-    edit.appendChild(editSpan)
-    combo.appendChild(edit)
-
-    const list = document.createElement('div')
-    list.classList = 'listOption'
-    const buttonEdit = document.createElement('button')
-    buttonEdit.classList = 'edit'
-    buttonEdit.innerHTML = "Edit"
-    const buttonDelete = document.createElement('button')
-    buttonDelete.classList = 'detele'
-    buttonDelete.innerHTML = "Delete"
-    list.appendChild(buttonEdit)
-    list.appendChild(buttonDelete)
-    combo.appendChild(list)
-
-    item.appendChild(combo)
-
-    edit.addEventListener('click', () => {
-    if (list.classList.contains("active")) {
-        list.classList.remove('active')
-    } else {
-        list.classList.add("active")
-    }
-    })
-
-    buttonDelete.addEventListener('click', () => {
-        task.deleteFromLocalStorage();
-        item.remove();
-    })
-    //edit button should create another kind of form
-    buttonEdit.addEventListener('click', () => {
-        item.style.display = "none"
-        createEditForm()
-
-        const editForm = document.getElementById('editTask')
-        const cancelButton = document.getElementById('editCancel')
-        cancelButton.addEventListener('click', () => {
-            editForm.remove()
-            item.style.display = null
-            list.classList.remove('active')
-        })
-
-        const editButton = document.getElementById('edit')
-        let titleInput = document.getElementById('inputEdit')
-        titleInput.value = h3.innerHTML
-        let describtionInput = document.getElementById('detailsEdit')
-        describtionInput.value = p.innerHTML
-
-        editButton.addEventListener('click', () => {
-            h3.innerHTML = titleInput.value
-            p.innerHTML = describtionInput.value
-            task.reName(`${titleInput.value}`)
-            task.reDes(`${describtionInput.value}`)
-            editForm.remove()
-            item.style.display = null
-            list.classList.remove('active')
-        })
-        //what if they press cancel ? we need to restore the item 
-    })
-    listItem.appendChild(item)
-}
-
-// Click Add will create a div + add object to localstorage
-formTask.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    let titleInput = document.getElementById('inputTitle') 
-    let describtionInput = document.getElementById('details')
-
-    const task = new Task(titleInput.value, describtionInput.value, false, false)
-    task.addToLocalStorage()
-
-    createItem(listItem, task)
-
-    titleInput.value = ""
-    describtionInput.value = ""
-    formTask.classList.add("hidden")
-})
-
-// const myIdea = localStorage.getItem('myIdea') == null ? localStorage.setItem('myIdea', "[]") : JSON.parse(localStorage.getItem('myIdea'))
-
-class Task {
+export class Task {
     constructor (title, describtion, done, important) {
         this.title = title
         this.describtion = describtion
@@ -402,19 +146,7 @@ class Task {
     }
 }
 
-let myTask = localStorage.getItem('myTask')
-if (myTask == null ) {
-    localStorage.setItem('myTask', "[]")
-} else {
-    myTask = JSON.parse(localStorage.getItem('myTask'))
-    for (let i = 0; i < myTask.length; i++) {
-        let task = new Task(myTask[i].title, myTask[i].describtion, myTask[i].done, myTask[i].important)   
-        createItem(listItem, task)
-    }
-}
-
-
-class Idea {
+export class Idea {
     constructor (title, describtion, done, important) {
         this.title = title
         this.describtion = describtion
@@ -488,44 +220,64 @@ class Idea {
         localStorage.setItem('myIdea', JSON.stringify(myIdea))
     }
 }
+//select right area
+const rightBody = document.querySelector('.right')
 
-const listIdea = document.getElementById('listIdea')
+const today = document.getElementById('Today');
+today.classList.add("active")
 
-let myIdea = localStorage.getItem('myIdea')
-if (myIdea == null ) {
-    localStorage.setItem('myIdea', "[]")
-} else {
-    myIdea = JSON.parse(localStorage.getItem('myIdea'))
-    for (let i = 0; i < myIdea.length; i++) {
-        let idea = new Idea(myIdea[i].title, myIdea[i].describtion, myIdea[i].done, myIdea[i].important)
-        createItem(listIdea, idea)
-    }
-}
+const todayTitle = document.createElement('h2')
+todayTitle.id = 'title'
+todayTitle.innerHTML = 'Today'
 
-const formIdea = document.getElementById('idea')
-formIdea.addEventListener('submit', (e) => {
-    e.preventDefault()
+rightBody.appendChild(todayTitle)
+rightBody.appendChild(addToday())
 
-    let titleInput = document.getElementById('titleIdea') 
-    let describtionInput = document.getElementById('subIdea')
-    
-    const idea = new Idea(titleInput.value, describtionInput.value, false, false)
-    console.log(idea)
-    idea.addToLocalStorage()
+today.addEventListener('click', () => {
 
-    createItem(listIdea, idea)
+    rightBody.innerHTML = ''
 
-    titleInput.value = ''
-    describtionInput.value = ''
-    formIdea.classList.add('hidden')
+    const todayTitle = document.createElement('h2')
+    todayTitle.id = 'title'
+    todayTitle.innerHTML = 'Today'
+
+    rightBody.appendChild(todayTitle)
+
+    rightBody.appendChild(addToday())
+
+    today.classList.add("active")
+    allTask.classList.remove("active")
+    allIdea.classList.remove("active")
+    important.classList.remove("active")
 })
 
-const buttonAddIdea = document.getElementById("addIdea")
-buttonAddIdea.addEventListener('click', () => {
-    formIdea.classList.remove('hidden')
-})
+// add eventlistener to create new right panel
+const allTask = document.getElementById('Task');
+allTask.addEventListener('click',  () => {
+    showAllTask()
+    today.classList.remove("active")
+    allTask.classList.add("active")
+    allIdea.classList.remove("active")
+    important.classList.remove("active")
+});
 
-const cancelIdeaButton = document.querySelector('input[id="idea"]')
-cancelIdeaButton.addEventListener('click', () => {
-    formIdea.classList.add('hidden')
-})
+const allIdea = document.getElementById('Idea');
+allIdea.addEventListener('click', () => {
+    showAllIdea();
+    today.classList.remove("active")
+    allTask.classList.remove("active")
+    allIdea.classList.add("active")
+    important.classList.remove("active")
+});
+
+const important = document.getElementById('Important');
+important.addEventListener('click', () => {
+    showImportant();
+    today.classList.remove("active")
+    allTask.classList.remove("active")
+    allIdea.classList.remove("active")
+    important.classList.add("active")
+});
+
+
+
---
+++ src/today.js
@@ -0,0 +1,376 @@
+import { Task, Idea } from "."
+//Create Form
+function createForm(type) {
+    const content = document.createElement('div')
+    content.classList = 'form'
+
+    const form = document.createElement('form')
+    form.id = `${type}`
+    form.classList = 'hidden'
+
+    const title = document.createElement('label')
+    title.innerHTML = 'Title'
+    const inputTitle = document.createElement('input')
+    inputTitle.type = 'text'
+    inputTitle.id = `${type}Title`
+    inputTitle.placeholder = 'What you gonna do ?'
+    form.appendChild(title)
+    form.appendChild(inputTitle)
+
+    const details = document.createElement('label')
+    details.innerHTML = 'Details'
+    const inputDetails = document.createElement('textarea')
+    inputDetails.id = `${type}Details`
+    inputDetails.rows = '2'
+    inputDetails.cols = '50'
+    inputDetails.placeholder = 'Anything that make you understand why you have to do it'
+    form.appendChild(details)
+    form.appendChild(inputDetails)
+
+    const twoButton = document.createElement('div')
+    twoButton.id = 'button'
+
+    const addButton = document.createElement('input')
+    addButton.type = 'submit'
+    addButton.id = `${type}Submit`
+    addButton.value = 'Add'
+
+
+
+    const cancelButton = document.createElement('input')
+    cancelButton.type = 'button'
+    cancelButton.id = `${type}Cancel`
+    cancelButton.value = 'Cancel'
+
+    cancelButton.addEventListener('click', () => {
+        form.classList.add('hidden')
+    })
+
+    twoButton.appendChild(addButton)
+    twoButton.appendChild(cancelButton)
+
+    form.appendChild(twoButton)
+    content.appendChild(form)
+
+    return content
+}
+
+function createAdd(type) {
+    const content = document.createElement('div')
+    content.classList = 'add'
+
+    const button = document.createElement('button')
+    button.id = `${type}Add`
+
+    const image = document.createElement('img')
+    image.src = "./images/plus.png"
+
+    const text = document.createElement('p')
+    text.innerHTML = `Add ${type}`
+
+    button.appendChild(image)
+    button.appendChild(text)
+
+    button.addEventListener('click', () => {
+        let form = document.getElementById(`${type}`)
+        form.classList.remove('hidden')
+    })
+
+    content.appendChild(button)
+    return content
+}
+
+function createLeftPanel() {
+    const content = document.createElement('div')
+    content.id = 'left-panel'
+
+    const form = createForm('task')
+    content.appendChild(form)
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        
+        let titleInput = document.getElementById('taskTitle') 
+        let describtionInput = document.getElementById('taskDetails')
+
+        const task = new Task(titleInput.value, describtionInput.value, false, false)
+        task.addToLocalStorage()
+
+        createItem(listTask, task)
+
+        titleInput.value = ""
+        describtionInput.value = ""
+        document.getElementById('task').classList.add("hidden")
+    })
+
+    const listTask = document.createElement('div')
+    listTask.id = 'listTask' 
+
+    avaialbleTask(listTask)
+    content.appendChild(listTask)
+
+    const addButton = createAdd('task')
+    content.appendChild(addButton)
+
+    return content
+}
+
+function createRightPanel() {
+    const content = document.createElement('div')
+    content.id = 'right-panel'
+
+    const form = createForm('idea')
+    content.appendChild(form)
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        
+        let titleInput = document.getElementById('ideaTitle') 
+        let describtionInput = document.getElementById('ideaDetails')
+
+        const idea = new Idea(titleInput.value, describtionInput.value, false, false)
+        idea.addToLocalStorage()
+
+        createItem(listIdea, idea)
+
+        titleInput.value = ""
+        describtionInput.value = ""
+        document.getElementById('idea').classList.add("hidden")
+    })
+
+    const listIdea = document.createElement('div')
+    listIdea.id = 'listIdea' 
+
+    availableIdea(listIdea)
+    content.appendChild(listIdea)
+
+    const addButton = createAdd('idea')
+    content.appendChild(addButton)
+
+    return content
+}
+
+function createItem(listItem, task) {
+
+    let newItem = (function newItem () {
+        const baseID = 'item'
+        let count = 0
+        return function (type = 'div') {
+            let item = document.createElement(type)
+            item.classList = baseID
+            item.id = count
+            count += 1
+            return item
+        }
+    }())
+
+    const item = newItem('div')
+    const itemID = Number(item.id)
+
+    const done = document.createElement('input')
+    done.type = "checkbox";
+    done.name = "done"
+    done.setAttribute('id', 'done')
+    item.appendChild(done)
+
+    done.defaultChecked = task.done == true ? true : false
+
+    //make button done change status
+    done.addEventListener('click', () => {
+        task.beDone()
+    })
+
+    let text = document.createElement('div')
+    text.setAttribute('id', 'text')
+    let h3 = document.createElement('h3')
+    h3.innerHTML = task.title
+    text.appendChild(h3)
+    let p = document.createElement('p')
+    p.innerHTML = task.describtion
+    text.appendChild(p)
+    item.appendChild(text)
+
+    let combo = document.createElement('div')
+    combo.id = "combo"
+
+    const star = document.createElement('div')
+    star.classList = 'star' 
+    const starBox = document.createElement('input')
+    starBox.type = "checkbox"
+    starBox.setAttribute('id', `important${itemID}`)
+    const starLabel = document.createElement('label')
+    starLabel.setAttribute('for', `important${itemID}`)
+    star.appendChild(starBox)
+    star.appendChild(starLabel)
+    combo.appendChild(star)
+
+    starBox.defaultChecked = task.important == true ? true : false
+
+    starBox.addEventListener('click', () => {
+        task.beImportant()
+    })
+
+    const edit = document.createElement('div')
+    edit.setAttribute('id', 'editContainer')
+    const editSpan = document.createElement('span')
+    editSpan.setAttribute('id', 'vert')
+    editSpan.classList = "material-symbols-outlined"
+    editSpan.textContent = "more_vert"
+    edit.appendChild(editSpan)
+    combo.appendChild(edit)
+
+    const list = document.createElement('div')
+    list.classList = 'listOption'
+    const buttonEdit = document.createElement('button')
+    buttonEdit.classList = 'edit'
+    buttonEdit.innerHTML = "Edit"
+    const buttonDelete = document.createElement('button')
+    buttonDelete.classList = 'detele'
+    buttonDelete.innerHTML = "Delete"
+    list.appendChild(buttonEdit)
+    list.appendChild(buttonDelete)
+    combo.appendChild(list)
+
+    item.appendChild(combo)
+
+    edit.addEventListener('click', () => {
+    if (list.classList.contains("active")) {
+        list.classList.remove('active')
+    } else {
+        list.classList.add("active")
+    }
+    })
+
+    buttonDelete.addEventListener('click', () => {
+        task.deleteFromLocalStorage();
+        item.remove();
+    })
+    //edit button should create another kind of form
+    buttonEdit.addEventListener('click', () => {
+        item.style.display = "none"
+        createEditForm()
+
+        const editForm = document.getElementById('editTask')
+        const cancelButton = document.getElementById('editCancel')
+        cancelButton.addEventListener('click', () => {
+            editForm.remove()
+            item.style.display = null
+            list.classList.remove('active')
+        })
+
+        const editButton = document.getElementById('edit')
+        let titleInput = document.getElementById('inputEdit')
+        titleInput.value = h3.innerHTML
+        let describtionInput = document.getElementById('detailsEdit')
+        describtionInput.value = p.innerHTML
+
+        editButton.addEventListener('click', () => {
+            h3.innerHTML = titleInput.value
+            p.innerHTML = describtionInput.value
+            task.reName(`${titleInput.value}`)
+            task.reDes(`${describtionInput.value}`)
+            editForm.remove()
+            item.style.display = null
+            list.classList.remove('active')
+        })
+        //what if they press cancel ? we need to restore the item 
+    })
+    listItem.appendChild(item)
+}
+
+function createEditForm() {
+    const place = document.querySelector('.form')
+    
+    const form = document.createElement('form')
+    form.id = "editTask"
+
+    const title = document.createElement('label')
+    title.setAttribute('for', 'title')
+    title.textContent = "Title:"
+    form.appendChild(title)
+
+    const input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('name', 'title')
+    input.setAttribute('placeholder', 'What you gonna do ?')
+    input.id = "inputEdit"
+    form.appendChild(input)
+
+    const details = document.createElement('label')
+    details.setAttribute('for', 'details')
+    details.textContent = "Details:"
+    form.appendChild(details)
+
+    const textArea = document.createElement('textarea')
+    textArea.setAttribute('rows', '2')
+    textArea.setAttribute('cols', '50')
+    textArea.setAttribute('name', 'details')
+    textArea.setAttribute('placeholder', 'Anything that make you understand why you have to do it')
+    textArea.id = "detailsEdit"
+    form.appendChild(textArea)
+
+    const twoButton = document.createElement('div')
+    twoButton.id = "button"
+
+    const submit = document.createElement('input')
+    submit.setAttribute('type', 'submit')
+    submit.setAttribute('name', 'submit')
+    submit.setAttribute('value', 'Edit')
+    submit.id = "edit"
+    twoButton.appendChild(submit)
+
+    const cancel = document.createElement('input')
+    cancel.setAttribute('type', 'button')
+    cancel.setAttribute('name', 'cancel')
+    cancel.setAttribute('value', 'Cancel')
+    cancel.id = "editCancel"
+    twoButton.appendChild(cancel)
+
+    form.appendChild(twoButton)
+    place.appendChild(form)
+}
+
+function avaialbleTask(list) {
+    let myTask = localStorage.getItem('myTask')
+    if (myTask == null ) {
+        localStorage.setItem('myTask', "[]")
+    } else {
+        myTask = JSON.parse(localStorage.getItem('myTask'))
+        for (let i = 0; i < myTask.length; i++) {
+            let task = new Task(myTask[i].title, myTask[i].describtion, myTask[i].done, myTask[i].important)   
+            createItem(list, task)
+        }
+    }
+}
+
+function availableIdea(list) {
+    let myIdea = localStorage.getItem('myIdea')
+    if (myIdea == null ) {
+        localStorage.setItem('myIdea', "[]")
+    } else {
+        myIdea = JSON.parse(localStorage.getItem('myIdea'))
+        for (let i = 0; i < myIdea.length; i++) {
+            let idea = new Idea(myIdea[i].title, myIdea[i].describtion, myIdea[i].done, myIdea[i].important)
+            createItem(list, idea)
+        }   
+    }
+}
+
+
+export function addToday(type) {
+    const content = document.createElement('div')
+    content.id = 'panel'
+
+    const left = createLeftPanel()
+    content.appendChild(left)
+
+    const right = createRightPanel()
+    content.appendChild(right)
+
+    return content
+}
+
+
+
+
+
